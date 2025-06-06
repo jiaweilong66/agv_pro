a7b75c31cb0b3c281ba1b3ece4785b96ba04b374
X-lanni <weijun.xie@elephantrobotics.com>
2025-06-06 16:15:07 +0800

    add slam_gmapping

---
+++ slam_gmapping/slam_gmapping/launch/slam_gmapping.launch.py
@@ -0,0 +1,31 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.substitutions import EnvironmentVariable
+import launch.actions
+import launch_ros.actions
+
+
+def generate_launch_description():
+    use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time', default='true')
+
+    rviz_config_dir = os.path.join(
+        get_package_share_directory('slam_gmapping'),
+        'rviz',
+        'gmapping.rviz')
+
+    return LaunchDescription([
+        launch_ros.actions.Node(
+            package='slam_gmapping', executable='slam_gmapping', output='screen', parameters=[{'use_sim_time':use_sim_time}]),
+    
+        launch_ros.actions.Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output='screen'),
+
+    ])
