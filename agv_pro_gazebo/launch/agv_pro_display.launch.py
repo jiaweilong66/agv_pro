6d3c37be135cf765214a2532a1dfc33f8c64bcf1
jiaweilong66 <627892495@qq.com>
2025-06-26 16:02:54 +0800

    add agv_pro_gazebo

--- agv_pro_gazebo/launch/agv_pro_display.launch.py
+++ agv_pro_gazebo/launch/agv_pro_display.launch.py
@@ -8,7 +8,6 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
 
     use_rviz = LaunchConfiguration('use_rviz', default='true')
-
     rviz_config_dir = os.path.join(
         get_package_share_directory('agv_pro_gazebo'),
         'rviz',
